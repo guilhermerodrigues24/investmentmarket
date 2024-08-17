164dfdc0c142361f69a09f8242f7713e3e084482
guilhermerodrigues24 <guilhermesp2409@gmail.com>
2024-08-17 14:54:26 -0300

    inicio

---
+++ src/main/java/com/backend/projetointegrador/ProjetoIntegradorSistemasDistribuidosApplication.java
@@ -0,0 +1,13 @@
+package com.backend.projetointegrador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoIntegradorSistemasDistribuidosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoIntegradorSistemasDistribuidosApplication.class, args);
+	}
+
+}
