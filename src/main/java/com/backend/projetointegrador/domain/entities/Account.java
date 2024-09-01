c7dd1d9ce8adc2a7dde9e600a4fd2dacdae2020b
Natanael Felix <f.natanael.felix.b@gmail.com>
2024-09-01 01:07:25 -0300

    chore: remove unused imports

--- src/main/java/com/backend/projetointegrador/domain/entities/Account.java
+++ src/main/java/com/backend/projetointegrador/domain/entities/Account.java
@@ -1,11 +1,9 @@
 package com.backend.projetointegrador.domain.entities;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.Getter;
--- src/main/java/com/backend/projetointegrador/domain/entities/Product.java
+++ src/main/java/com/backend/projetointegrador/domain/entities/Product.java
@@ -1,6 +1,5 @@
 package com.backend.projetointegrador.domain.entities;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
