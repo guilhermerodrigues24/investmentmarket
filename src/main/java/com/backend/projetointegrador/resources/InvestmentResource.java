785d98bacf7ca30ac8746c2721a4d62062c1c498
Natanael Felix <f.natanael.felix.b@gmail.com>
2024-09-17 01:31:57 -0300

    fix: add missing methods

--- src/main/java/com/backend/projetointegrador/resources/InvestmentResource.java
+++ src/main/java/com/backend/projetointegrador/resources/InvestmentResource.java
@@ -3,18 +3,31 @@ package com.backend.projetointegrador.resources;
 import com.backend.projetointegrador.domain.dtos.InvestmentRequestDTO;
 import com.backend.projetointegrador.domain.dtos.InvestmentResponseDTO;
 import com.backend.projetointegrador.services.InvestmentService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.List;
 
 @RestController
-@RequestMapping("/investment")
+@RequestMapping("/investments")
+@RequiredArgsConstructor
 public class InvestmentResource {
+    private final InvestmentService investmentService;
 
-    private InvestmentService investmentService;
+    @GetMapping
+    public ResponseEntity<List<InvestmentResponseDTO>> findAll() {
+        return ResponseEntity.ok().body(investmentService.findAll());
+    }
 
     @GetMapping("/{id}")
     public ResponseEntity<InvestmentResponseDTO> findById(@PathVariable Long id) {
@@ -25,7 +38,7 @@ public class InvestmentResource {
     @PostMapping
     public ResponseEntity<InvestmentResponseDTO> create(@RequestBody InvestmentRequestDTO investmentRequestDTO, Authentication authentication) throws URISyntaxException {
         InvestmentResponseDTO responseDTO = investmentService.create(investmentRequestDTO, authentication);
-        return ResponseEntity.created(new URI("/investment/" + responseDTO.id())).body(responseDTO);
+        return ResponseEntity.created(new URI("/investments/" + responseDTO.id())).body(responseDTO);
     }
 
     @DeleteMapping("/{id}")
--- src/main/java/com/backend/projetointegrador/resources/ProductResource.java
+++ src/main/java/com/backend/projetointegrador/resources/ProductResource.java
@@ -3,21 +3,32 @@ package com.backend.projetointegrador.resources;
 import com.backend.projetointegrador.domain.dtos.ProductRequestDTO;
 import com.backend.projetointegrador.domain.dtos.ProductResponseDTO;
 import com.backend.projetointegrador.services.ProductService;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.List;
 
 @RestController
 @RequestMapping("/products")
+@RequiredArgsConstructor
 public class ProductResource {
+    private final ProductService productService;
 
-    @Autowired
-    private ProductService service;
-    @Autowired
-    private ProductService productService;
+    @GetMapping
+    public ResponseEntity<List<ProductResponseDTO>> findAll() {
+        List<ProductResponseDTO> responseDTO = productService.findAll();
+        return ResponseEntity.ok().body(responseDTO);
+    }
 
     @GetMapping("/{id}")
     public ResponseEntity<ProductResponseDTO> findById(@PathVariable Long id) {
