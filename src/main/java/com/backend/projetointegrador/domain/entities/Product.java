11f12863ab7ecb11df8b6c143164780583106cf6
Natanael Felix <f.natanael.felix.b@gmail.com>
2024-10-10 17:26:46 -0300

    chore: remove unused code and imports

--- src/main/java/com/backend/projetointegrador/domain/entities/Product.java
+++ src/main/java/com/backend/projetointegrador/domain/entities/Product.java
@@ -4,17 +4,14 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
-import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.io.Serial;
 import java.io.Serializable;
-import java.time.Instant;
 import java.util.Objects;
 import java.util.Set;
 
